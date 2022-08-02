e18a1191798e9f468a159611b99ca41816b2201d
Alejandro <sancha0701@gmail.com>
2022-08-02 19:53:33 -0400

    commit tree 2113c80b4ea38d1415bc4e17c78184edc441ab90

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+const node = document.createTextNode('victory');
+newHeader.id = "victory";
+
+//newHeader.h1 = "h1#vicotry"
+newHeader.textContent = "is the champion";
